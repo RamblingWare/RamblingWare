2b2c17d034cac4738eec5ca901575d761b13a5b2
amdelamar <amdelamar@protonmail.com>
2017-06-27 22:38:10 -0400

    Add more unit tests

--- src/test/java/com/rw/test/UtilsTests.java
+++ src/test/java/com/rw/test/UtilsTests.java
@@ -1,5 +1,6 @@
 package com.rw.test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -22,7 +23,9 @@ public class UtilsTests {
     public void test() {
 
         String time = Utils.formatTime(System.currentTimeMillis());
-
+        assertNotNull(time);
+        
+        time = Utils.formatLong(System.currentTimeMillis());
         assertNotNull(time);
     }
 
@@ -35,26 +38,54 @@ public class UtilsTests {
         String[] expected = {"0", "5", "999", "1k", "-5.8k", "10k", "-101k", "2M", "-7.8M", "92M",
                 "123M", "9.9M", "999P", "1.2P", "-9.2E", "9.2E"};
         for (int i = 0; i < numbers.length; i++) {
-            long n = numbers[i];
-            String formatted = Utils.formatLong(n);
-            System.out.println(n + " => " + formatted);
-            if (!formatted.equals(expected[i]))
+            long nm = numbers[i];
+            String formatted = Utils.formatLong(nm);
+            System.out.println(nm + " => " + formatted);
+            if (!formatted.equals(expected[i])) {
                 throw new AssertionError("Expected: " + expected[i] + " but found: " + formatted);
+            }
         }
 
     }
-    
+
     @Test
     public void emailFormat() {
         assertTrue(Utils.isValidEmail("jdoe@gmail.com"));
         assertTrue(Utils.isValidEmail("jdoe@yahoo.com"));
         assertTrue(Utils.isValidEmail("jdoe@us.example.com"));
         assertTrue(Utils.isValidEmail("j.doe@gmail.com"));
-        
+
         assertTrue(!Utils.isValidEmail("jdoe@@gmail.com"));
         assertTrue(!Utils.isValidEmail("@gmail.com"));
         assertTrue(!Utils.isValidEmail("jdoe@"));
         assertTrue(!Utils.isValidEmail("jdoe@gmail."));
         assertTrue(!Utils.isValidEmail(""));
+    }
+
+    @Test
+    public void dateTests() {
+
+        String td = "June 27 2017";
+        java.util.Date date = Utils.convertStringToDate(td);
+        
+        assertNotNull(date);
+        assertNotNull(Utils.getDate());
+        assertNotNull(Utils.formatReadableDate(date));
+        assertNotNull(Utils.formatReadableDateTime(date));
+        assertNotNull(Utils.formatMySQLDate(date));
+        assertNotNull(Utils.formatSQLServerDate(date));        
+    }
+    
+    @Test
+    public void stringTests() {
+        
+        String temp = "  t e st  ";
+        assertEquals(Utils.removeAllSpaces(temp),"test");
+        
+        temp = "/t@e's?t";
+        assertEquals(Utils.removeBadChars(temp),"test");
+        
+        temp = "te\r\nst";
+        assertEquals(Utils.removeNonAsciiChars(temp),"test");
     }
 }
