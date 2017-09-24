98d6f4b60c20b4efb9e00b256bf5c1fb1aa9a95d
amdelamar <amdelamar@protonmail.com>
2017-09-24 18:46:40 -0400

    Set version 1.0.0

--- src/main/java/com/rant/database/CouchDBSetup.java
+++ src/main/java/com/rant/database/CouchDBSetup.java
@@ -50,7 +50,7 @@ public class CouchDBSetup extends DatabaseSetup {
         try {
             CloudantClient client = getConnection();
 
-            if (!client.serverVersion().startsWith("2.0")) {
+            if (!client.serverVersion().startsWith("2.0.0")) {
                 throw new InvalidClassException("Failed Database Test: Unexpected CouchDB Version ("
                         + client.serverVersion() + ") was expecting (2.0.0).");
             }
