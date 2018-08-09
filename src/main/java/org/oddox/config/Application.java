64ec7069b524c952a34e9d22e0237da99c1b7bf7
amdelamar <amdelamar@protonmail.com>
2018-08-08 21:07:04 -0400

    Remove application check for null dbUrl

--- src/main/java/org/oddox/config/Application.java
+++ src/main/java/org/oddox/config/Application.java
@@ -137,11 +137,6 @@ public final class Application {
         try {
             // try read docker env variables
             String dbUrl = env.get("DB_URL");
-            
-            if(dbUrl == null || dbUrl.isEmpty()) {
-                throw new IOException("DB_URL is invalid or empty");
-            }
-            
             db.setUrl(dbUrl);
             
             if(dbUrl.contains("https")) {
@@ -158,7 +153,6 @@ public final class Application {
             
             // custom port used?
             if(host.contains(":")) {
-                
                 db.setPort(host.substring(host.indexOf(":") + 1, host.length()));
                 host = host.substring(0, host.indexOf(":"));
             }
